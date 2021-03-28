a06d1acd9491b722d5cb58c03e912d66d17ae190
samuel <samuelbaubas@gmail.com>
2021-03-28 14:16:46 -0600

    Primer commit de app para validación de formularios

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        _crearFondo(context),
+      ],
+    ));
+  }
+
+  Widget _crearFondo(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    final fondoMorado = Container(
+      height: size.height * 0.40,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: <Color>[
+        Color.fromRGBO(63, 63, 156, 1.0),
+        Color.fromRGBO(90, 70, 178, 1.0)
+      ])),
+    );
+
+    final circulo = Container(
+      width: 100.00,
+      height: 100.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Color.fromRGBO(255, 255, 255, .05)),
+    );
+
+    return Stack(
+      children: [
+        fondoMorado,
+        Positioned(child: circulo, top: 90.0, left: 30.0),
+        Positioned(child: circulo, top: -40.0, right: -30.0),
+        Positioned(child: circulo, bottom: -50.0, right: -10.0),
+        Positioned(child: circulo, bottom: 120.0, right: 20.0),
+        Positioned(child: circulo, bottom: -50.0, left: -20.0),
+
+        //TODO:terminar la clase con lo que estabamos haciendo en flutter curso básico
+        // Container(
+        //   child: Column,
+        // )
+      ],
+    );
+  }
+}
